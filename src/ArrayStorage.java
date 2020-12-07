b627e9edf17fb12500935ce18c542d816f898f12
kavalex <aleksejkaverzin@gmail.com>
2020-12-07 16:18:11 +0700

    HW1, implement methods in the class ArrayStorage Implement methods: save(), get(), delete(), clear(), getAll(), size()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,61 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        if (size > 0) {
+            for (int i = 0; i <= size; i++) {
+                storage[i] = null;
+            }
+            size = 0;
+        }
     }
 
     void save(Resume r) {
+        if (size == storage.length) {
+            System.out.println("Sorry, base is full. The resume did not save.");
+        } else {
+            storage[size] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
+        System.out.println("Sorry, resume \"" + uuid + "\" did not found.");
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+                storage[size] = null;
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] storageWithout0 = new Resume[size];
+        System.arraycopy(storage, 0, storageWithout0, 0, size);
+        return storageWithout0;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
